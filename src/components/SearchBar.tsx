a9c5bbaa4fa5c8a0cb34602e71c58de9b4159abe
Andre Fonseca <andrefonseca.aff@gmail.com>
2025-12-02 13:27:07 +0000

    Implement debounce on search query to prevent excessive API requests and small fixes in UI

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { router } from "../routes/router";
 
 const SearchBar = () => {
   const [isVisible, setIsVisible] = useState(false);
+
   const inputRef = useRef<HTMLInputElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -54,7 +55,7 @@ const SearchBar = () => {
     <div ref={containerRef} className="relative">
       <div className="flex items-center gap-2">
         {isVisible && (
-          <div className="flex items-center bg-gray-800 text-white rounded overflow-hidden shadow">
+          <div className="flex items-center bg-gray-800 text-white rounded overflow-hidden shadow max-w-[160px] sm:max-w-none ml-2 sm:ml-0">
             <LucideSearch className="ml-2 w-5 h-5 text-gray-400" />
             <input
               type="text"
@@ -62,20 +63,24 @@ const SearchBar = () => {
               value={query}
               onChange={handleChangeQuery}
               placeholder="Movie Title..."
-              className="flex-1 bg-transparent px-2 py-1 text-white outline-none"
+              className=" flex-1 bg-transparent px-2 py-1 text-white outline-none max-w-[160px]msm:max-w-none"
               autoFocus
             />
           </div>
         )}
-        <button
-          className="p-2 rounded text-white hover:bg-gray-800 active:scale-95 active:opacity-50 transition-transform cursor-pointer"
-          aria-label="Toggle search input"
-          onClick={() => {
-            setIsVisible(!isVisible);
-          }}
-        >
-          <LucideSearch size={20} />
-        </button>
+        <div className="w-5 h-5 flex items-center justify-center ml-3">
+          {query.trim().length === 0 && (
+            <button
+              className="p-2 rounded text-white hover:bg-gray-800 active:scale-95 active:opacity-50 transition-transform cursor-pointer"
+              aria-label="Toggle search input"
+              onClick={() => {
+                setIsVisible(!isVisible);
+              }}
+            >
+              <LucideSearch size={20} />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import type { Movie } from "@/types";
 import MovieCard from "@/components/MovieCard";
 import { useMovies } from "@/hooks/useMovies";
+import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/search")({
   component: SearchComponent,
@@ -9,9 +10,24 @@ export const Route = createFileRoute("/search")({
 
 function SearchComponent() {
   const { query } = Route.useSearch();
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
+
   console.log("Search Query", query);
 
-  const { data: results = [], isLoading, error } = useMovies(query || "");
+  //Using to make debounce in search query (this prevents spam from reaching the server and improves performance)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 500); // 0.5s debounce
+
+    return () => clearTimeout(timeout);
+  }, [query]);
+
+  const {
+    data: results = [],
+    isLoading,
+    error,
+  } = useMovies(debouncedQuery || "");
 
   if (isLoading) {
     return (
@@ -31,9 +47,14 @@ function SearchComponent() {
 
   console.log("Search Results", results);
   return (
-    <div>
+    <div className="w-full px-4 mt-4">
+      {query && (
+        <h2 className="text-xl font-semibold mb-4 ml-3">
+          Search results for <span className="">"{debouncedQuery}"</span>
+        </h2>
+      )}
       {results.length > 0 ? (
-        <ul className="flex overflow-x-scroll overflow-y-visible space-x-11 px-4 md:px-6 py-4 scrollbar-hide relative">
+        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 justify-items-center">
           {results.map((movie: Movie, index) => (
             <li key={movie.id} className="relative">
               <MovieCard movie={movie} position={index + 1} />
